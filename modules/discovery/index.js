837911ced4cbd8dea292972b82e280253a93ec77
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-18 01:13:54 -0700

    Create index.js

---
+++ modules/discovery/index.js
@@ -0,0 +1,11 @@
+import target from "./target.js";
+import reflect from "./reflect.js";
+import update from "./update.js";
+import synthesis from "./synthesis.js";
+
+export {
+  target,
+  reflect,
+  update,
+  synthesis
+};
