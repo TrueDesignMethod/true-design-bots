a8134e8db3218eb2685f29ab65adf063e3bbc95b
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-17 22:58:20 -0700

    Update synthesis.js

--- modules/alignment/synthesis.js
+++ modules/alignment/synthesis.js
@@ -1,1 +1,13 @@
+// modules/alignment/synthesis.js
+export const alignmentSynthesis = {
+stage: "alignment",
+name: "ALIGNMENT_SYNTHESIS",
+requiresPro: true,
+tokenCeiling: 360,
+prompt: ({ context }) => `The user has requested an Alignment LifePrint.
+Integrate simplification, iteration, growth, and nurture into a resilience-focused snapshot.
+Do not introduce new goals unless asked.
 
+
+Context:\n${context}`
+};
