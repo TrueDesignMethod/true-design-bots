91669e6c17f0698a6012576a566aa15a34cb9ae6
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-18 01:14:52 -0700

    Create index.js

---
+++ modules/alignment/index.js
@@ -0,0 +1,15 @@
+import simplify from "./simplify.js";
+import reduceRemoveDelegate from "./reduceRemoveDelegate.js";
+import iterate from "./iterate.js";
+import grow from "./grow.js";
+import nurture from "./nurture.js";
+import synthesis from "./synthesis.js";
+
+export {
+  simplify,
+  reduceRemoveDelegate,
+  iterate,
+  grow,
+  nurture,
+  synthesis
+};
