3c75d3ef95aaf339992432e1a4b5aa52b6c6c0ad
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-18 02:36:13 -0700

    Update index.js

--- modules/discovery/index.js
+++ modules/discovery/index.js
@@ -1,11 +1,20 @@
-import target from "./target.js";
-import reflect from "./reflect.js";
-import update from "./update.js";
-import synthesis from "./synthesis.js";
+// modules/discovery/index.js
+// Exports all Discovery modules for TRUE
 
-export {
-  target,
-  reflect,
-  update,
-  synthesis
-};
+import { target } from "./target.js";
+import { reflect } from "./reflect.js";
+import { update } from "./update.js";
+import { synthesis } from "./synthesis.js";
+
+// Ensure each module has a buildPrompt method
+const modules = { target, reflect, update, synthesis };
+
+for (const key in modules) {
+  if (!modules[key].buildPrompt) {
+    modules[key].buildPrompt = ({ input, messages }) =>
+      modules[key].prompt({ userInput: input, messages });
+  }
+}
+
+export { target, reflect, update, synthesis };
+export default modules;
