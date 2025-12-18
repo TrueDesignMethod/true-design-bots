e50300bff01a2657b0edc3918d6c06727a2e256a
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-17 22:56:48 -0700

    Update iterate.js

--- modules/alignment/iterate.js
+++ modules/alignment/iterate.js
@@ -1,1 +1,13 @@
+// modules/alignment/iterate.js
+export const iterate = {
+stage: "alignment",
+name: "ITERATE",
+requiresPro: true,
+tokenCeiling: 260,
+prompt: ({ userInput }) => `You are guiding the user through Iterate in True Alignment.
+Help them revise goals compassionately as they evolve.
+Remove shame from change.
 
+
+User input:\n"${userInput}"`
+};
