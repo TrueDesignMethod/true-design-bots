0a2c37eb8bc342cf998263c54aa1b0dc368c7f91
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-17 22:56:12 -0700

    Update reduceRemoveDelegate.js

--- modules/alignment/reduceRemoveDelegate.js
+++ modules/alignment/reduceRemoveDelegate.js
@@ -1,1 +1,13 @@
+// modules/alignment/reduceRemoveDelegate.js
+export const reduceRemoveDelegate = {
+stage: "alignment",
+name: "REDUCE_REMOVE_DELEGATE",
+requiresPro: false,
+tokenCeiling: 300,
+prompt: ({ userInput }) => `Help the user decide what they are ready to reduce, remove, or delegate.
+Respect readiness.
+Avoid moral pressure.
 
+
+User input:\n"${userInput}"`
+};
