f19da9215cc47323cd5c984b23544eb3e3afd517
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-17 22:54:57 -0700

    Update synthesis.js

--- modules/planning/synthesis.js
+++ modules/planning/synthesis.js
@@ -1,1 +1,13 @@
+// modules/planning/synthesis.js
+export const planningSynthesis = {
+stage: "planning",
+name: "PLANNING_SYNTHESIS",
+requiresPro: true,
+tokenCeiling: 360,
+prompt: ({ context }) => `The user has requested a Planning LifePrint.
+Summarize goals, actions, habits, obstacles, and systems using the user’s language.
+Do not advance into Alignment unless invited.
 
+
+Context:\n${context}`
+};
