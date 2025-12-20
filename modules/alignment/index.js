113bca2a206d69dba4608fbc269d6cb6951186f0
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-20 02:28:36 -0700

    Update index.js

--- modules/alignment/index.js
+++ modules/alignment/index.js
@@ -1,6 +1,3 @@
-// modules/alignment/index.js
-// Exports all Alignment modules for TRUE
-
 import { simplify } from "./simplify.js";
 import { reduceRemoveDelegate } from "./reduceRemoveDelegate.js";
 import { iterate } from "./iterate.js";
@@ -8,14 +5,11 @@ import { grow } from "./grow.js";
 import { nurture } from "./nurture.js";
 import { synthesis } from "./synthesis.js";
 
-const modules = { simplify, reduceRemoveDelegate, iterate, grow, nurture, synthesis };
-
-for (const key in modules) {
-  if (!modules[key].buildPrompt) {
-    modules[key].buildPrompt = ({ input, messages }) =>
-      modules[key].prompt({ userInput: input, messages });
-  }
-}
-
-export { simplify, reduceRemoveDelegate, iterate, grow, nurture, synthesis };
-export default modules;
+export default {
+  simplify,
+  reduceRemoveDelegate,
+  iterate,
+  grow,
+  nurture,
+  synthesis
+};
