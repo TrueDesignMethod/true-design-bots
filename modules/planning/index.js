5b2d485cc158acae2eb5f931d9104de717146afb
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-20 02:25:24 -0700

    Update index.js

--- modules/planning/index.js
+++ modules/planning/index.js
@@ -1,41 +1,19 @@
-// modules/planning/index.js
-// Exports all Planning modules for TRUE
-
+import { actionDesign } from "./actionDesign.js";
 import { goalPrioritization } from "./goalPrioritization.js";
 import { goalRefinement } from "./goalRefinement.js";
-import { actionDesign } from "./actionDesign.js";
 import { obstacleMapping } from "./obstacleMapping.js";
 import { plan7 } from "./plan7.js";
 import { plan30 } from "./plan30.js";
 import { plan90 } from "./plan90.js";
 import { synthesis } from "./synthesis.js";
 
-const modules = {
+export default {
+  actionDesign,
   goalPrioritization,
   goalRefinement,
-  actionDesign,
   obstacleMapping,
   plan7,
   plan30,
   plan90,
-  synthesis,
+  synthesis
 };
-
-for (const key in modules) {
-  if (!modules[key].buildPrompt) {
-    modules[key].buildPrompt = ({ input, messages }) =>
-      modules[key].prompt({ userInput: input, messages });
-  }
-}
-
-export {
-  goalPrioritization,
-  goalRefinement,
-  actionDesign,
-  obstacleMapping,
-  plan7,
-  plan30,
-  plan90,
-  synthesis,
-};
-export default modules;
