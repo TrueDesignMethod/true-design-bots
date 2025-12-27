5963448fa58577c5f01863ff3a3cdf5230ed59b3
TrueDesignMethod <truedesignmethod@gmail.com>
2025-12-27 04:04:07 -0500

    Update supabase.js

--- public/supabase.js
+++ public/supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://www.truedesign.io/",
+  "https://edmqbqjtvtnttjtgyuis.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbXFicWp0dnRudHRqdGd5dWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwNjU5NjUsImV4cCI6MjA4MDY0MTk2NX0.GGk_1NAqJff1eLXaBR9lRvlNU4yEg-BcHTU9SQWMzRs"
+
 );
