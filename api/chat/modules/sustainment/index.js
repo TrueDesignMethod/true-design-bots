e6e622cb0b48184e5d9047a4cfc9ea8b9e0e326a
TrueDesignMethod <truedesignmethod@gmail.com>
2026-02-07 11:38:31 -0700

    Update index.js

--- api/chat/modules/sustainment/index.js
+++ api/chat/modules/sustainment/index.js
@@ -4,12 +4,18 @@
 import execute from "./execute.js";
 import discipline from "./discipline.js";
 import evaluate from "./evaluate.js";
+import plan7 from "./plan7.js";
+import plan30 from "./plan30.js";
+import plan90 from "./plan90.js";
 
 const sustainment = Object.freeze({
   index: execute,   // default Sustainment entry point
   execute,
   discipline,
-  evaluate
+  evaluate,
+  plan7,
+  plan30,
+  plan90
 });
 
 export default sustainment;
