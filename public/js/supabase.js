9bda12bfa5dbc50b24926cbf876d3d4b7e6b750b
TrueDesignMethod <truedesignmethod@gmail.com>
2026-05-16 13:56:21 -0600

    Update supabase.js

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -12,8 +12,7 @@ const SUPABASE_URL =
   "https://qtudeevnhyauhcrixbox.supabase.co/rest/v1/";
 
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0dWRlZXZuaHlhdWhjcml4Ym94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NDM0NDksImV4cCI6MjA5NDUxOTQ0OX0.JCgysocQdbfsYtLbVPj0dxjEuo3OGErn65-lWDEv6Tw
-";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0dWRlZXZuaHlhdWhjcml4Ym94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NDM0NDksImV4cCI6MjA5NDUxOTQ0OX0.JCgysocQdbfsYtLbVPj0dxjEuo3OGErn65-lWDEv6Tw";
 
 
 // --------------------------------------------------
