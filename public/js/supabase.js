ac6efd110e9e8324685e860afb4668f569f8f69d
TrueDesignMethod <truedesignmethod@gmail.com>
2026-05-16 14:03:20 -0600

    Update supabase.js

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -1,1 +1,10 @@
-console.log("supabase test");
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://qtudeevnhyauhcrixbox.supabase.co/rest/v1/";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0dWRlZXZuaHlhdWhjcml4Ym94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NDM0NDksImV4cCI6MjA5NDUxOTQ0OX0.JCgysocQdbfsYtLbVPj0dxjEuo3OGErn65-lWDEv6Tw";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
